186f80b9444cf885bcde087673a80c813014986c
williamandres <williamaguilar@misena.edu.co>
2021-04-28 14:29:13 -0500

    exercise1

---
+++ script.js
@@ -0,0 +1,26 @@
+var Alumno = /** @class */ (function () {
+    function Alumno(nombre, edad, correo, promedio) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+        this.promedio = promedio;
+    }
+    return Alumno;
+}());
+var alumnos = [
+    { nombre: "Jose Perez", edad: 22, correo: "jose@perez.com", promedio: 6 },
+    { nombre: "Juan Perez", edad: 19, correo: "juan@perez.com", promedio: 7 },
+    { nombre: "Maria Yepez", edad: 21, correo: "maria@yepez.com", promedio: 9 },
+    { nombre: "Jose Uribe", edad: 23, correo: "jose@gmail.com", promedio: 5 },
+    { nombre: "Juana Medina", edad: 18, correo: "juana@gmail.com", promedio: 7 },
+    { nombre: "Maria Contreras", edad: 20, correo: "mariaC@gmail.com", promedio: 10 },
+    { nombre: "Alan Brito", edad: 22, correo: "alan@brito.com", promedio: 5 },
+    { nombre: "Angelica Galindo", edad: 19, correo: "angelica@galindo.com", promedio: 7 },
+    { nombre: "Sara Castro", edad: 21, correo: "scastro@gmail.com", promedio: 6 },
+    { nombre: "Carlos Marin", edad: 23, correo: "carlos@gmail.com", promedio: 8 },
+    { nombre: "Karla Medina", edad: 18, correo: "Karla@gmail.com", promedio: 3 },
+    { nombre: "Daniela Medina", edad: 20, correo: "danielamedinaC@gmail.com", promedio: 7 }
+];
+// 1.Devuelva una arreglo que contenga todos los estudiantes cuyo promedio es mayor a 6 puntos.
+var mayor6 = alumnos.filter(function (alumnos) { return alumnos.promedio > 6; });
+console.log(mayor6);
--- script.ts
+++ script.ts
@@ -28,3 +28,7 @@ class Alumno {
     { nombre: "Daniela Medina", edad: 20, correo: "danielamedinaC@gmail.com", promedio: 7 }
 ]
 
+
+// 1.Devuelva una arreglo que contenga todos los estudiantes cuyo promedio es mayor a 6 puntos.
+const mayor6 = alumnos.filter(alumnos => alumnos.promedio > 6)
+console.log(mayor6)
